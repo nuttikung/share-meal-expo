5f4f008bbf622e3ebb97a84c0aeca39d79bb734b
Phudit Chuengjaroen <phudit.nut@gmail.com>
2025-04-30 00:03:44 +0700

    feat: refactor type

--- context/app/app-context-provider.tsx
+++ context/app/app-context-provider.tsx
@@ -1,12 +1,19 @@
-import { TBill } from "@/types/bill";
+import type { TBill, TMember } from "@/types";
 import { createContext, PropsWithChildren, useState } from "react";
 
 // ----------------------------------------------------------------------
 
 type AppContextProps = {
   bills: Array<TBill>;
+  members: Array<TMember>;
   // ----------------------------------------------------------------------
   onAddBill: (bill: TBill) => void;
+  onUpdateBill: (bill: TBill) => void;
+  onRemoveBill: (id: TBill["id"]) => void;
+  // ----------------------------------------------------------------------
+  onAddMember: (member: TMember) => void;
+  onUpdateMember: (member: TMember) => void;
+  onRemoveMember: (id: TMember["id"]) => void;
 };
 
 // ----------------------------------------------------------------------
@@ -17,6 +24,7 @@ const AppContext = createContext<AppContextProps | null>(null);
 
 function AppContextProvider({ children }: PropsWithChildren) {
   const [bills, setBills] = useState<AppContextProps["bills"]>([]);
+  const [members, setMembers] = useState<AppContextProps["members"]>([]);
 
   // ----------------------------------------------------------------------
 
@@ -24,11 +32,49 @@ function AppContextProvider({ children }: PropsWithChildren) {
     setBills((prev) => [...prev, bill]);
   };
 
+  const onUpdateBill = (bill: TBill) => {
+    setBills((prev) =>
+      prev.map((record) =>
+        record.id === bill["id"] ? { ...record, ...bill } : record,
+      ),
+    );
+  };
+
+  const onRemoveBill = (id: TBill["id"]) => {
+    setBills((prev) => prev.filter((record) => record.id !== id));
+  };
+
+  // ----------------------------------------------------------------------
+
+  const onAddMember = (member: TMember) => {
+    setMembers((prev) => [...prev, member]);
+  };
+
+  const onUpdateMember = (member: TMember) => {
+    setMembers((prev) =>
+      prev.map((record) =>
+        record.id === member["id"] ? { ...record, ...member } : record,
+      ),
+    );
+  };
+
+  const onRemoveMember = (id: TMember["id"]) => {
+    setMembers((prev) => prev.filter((record) => record.id !== id));
+  };
+
+  // ----------------------------------------------------------------------
+
   return (
     <AppContext.Provider
       value={{
         bills,
+        members,
         onAddBill,
+        onUpdateBill,
+        onRemoveBill,
+        onAddMember,
+        onUpdateMember,
+        onRemoveMember,
       }}
     >
       {children}
---
+++ types/index.ts
@@ -0,0 +1,3 @@
+export * from "./bill";
+export * from "./member";
+export * from "./order";
